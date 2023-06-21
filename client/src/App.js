796b8f8f6e028d7b227137ae82172495b4a18b59
Florian Bär <florianbar01@gmail.com>
2023-06-21 21:28:49 +0200

    added apollo client library

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
 import MovieList from './components/MovieList';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache()
+});
 
 function App() {
   return (
-    <div className="App">
-      <h1>Watch List</h1>
-      <MovieList />
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>Watch List</h1>
+        <MovieList />
+      </div>
+    </ApolloProvider>
   );
 }
 
